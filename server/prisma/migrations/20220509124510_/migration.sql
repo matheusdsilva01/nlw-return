165894c227a6ef5222c63755fd859563c3582a56
Matheus Silva <ms25022003@gmail.com>
2022-05-09 09:45:37 -0300

    Add migrations

---
+++ server/prisma/migrations/20220509124510_/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE `feedbacks` (
+    `id` VARCHAR(191) NOT NULL,
+    `type` VARCHAR(191) NOT NULL,
+    `comment` VARCHAR(191) NOT NULL,
+    `screenshot` TEXT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
